4add97ca635f69c673a22edded42613ff03fe70c
Anjali <anjalisheel@microsoft.com>
2023-11-27 12:43:15 +0530

    Moved Drts

--- src/Test/WindowsFormsIntegration/DRT/WindowsFormsIntegration/DrtWindowsFormsIntegration.cs
+++ src/Test/WindowsFormsIntegration/DRT/WindowsFormsIntegration/DrtWindowsFormsIntegration.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -32,8 +36,8 @@ public class DrtWindowsFormsIntegration : DrtBase
     private DrtWindowsFormsIntegration() 
     {
         WindowTitle = "WindowsFormsIntegration DRT";
-        Contact = "scoberry";
-        TeamContact = "uifxcdev";
+        Contact = "Microsoft";
+        TeamContact = "WPF";
         DrtName = "DrtWindowsFormsIntegration";
         Suites = new DrtTestSuite[] 
         {
